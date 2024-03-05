ce81ae256ca04d08510330bbd7b1a6cc81dcebec
Jeroen Tiels <jeroentiels@gmail.com>
2024-03-05 16:22:32 +0100

    qdd ;ove;ent to plqyer one

--- scripts/Player2.cs
+++ scripts/Player2.cs
@@ -11,5 +11,20 @@ public partial class Player2 : Sprite2D
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
+		movement();
+	}
+
+	public void movement()
+	{
+		Godot.Sprite2D child = this.GetNode<Godot.Sprite2D>("ChildNode");
+		float amount = -4;
+		if (Input.IsKeyPressed(Key.S))
+		{
+			this.Position += new Vector2(0, -amount);
+		}
+		if (Input.IsKeyPressed(Key.W))
+		{
+			this.Position += new Vector2(0, amount);
+		}
 	}
 }
