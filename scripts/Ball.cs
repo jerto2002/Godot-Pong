9438b26063759671b310f90aaf84eb3df6f5ad61
Jeroen Tiels <jeroentiels@gmail.com>
2024-03-17 17:08:51 +0100

    implement scoring

--- scripts/Ball.cs
+++ scripts/Ball.cs
@@ -3,17 +3,17 @@ using System;
 
 public partial class Ball : StaticBody2D
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-	}
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+    }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta)
+    {
+    }
 
-	private Vector2 _velocity = new Vector2(250, 250);
+    private Vector2 _velocity = new Vector2(250, 250);
 
     public override void _PhysicsProcess(double delta)
     {
@@ -30,14 +30,41 @@ public partial class Ball : StaticBody2D
 
             Node collidedNode = (Node)collision.GetCollider();
 
-            if (collidedNode is CharacterBody2D staticBody)
+            if (collidedNode is StaticBody2D staticBody2D) //check for goal
+            {
+                // Get the groups of the collided node
+                var collidedGroups = staticBody2D.GetGroups();
+
+                // Print the groups
+
+                if (collidedGroups.Contains("Goal1"))
+                {
+                    GD.Print("Goal1");
+                    Label label = GetNode<Label>("../ScorePlayer1");
+                    // Parse the current text and increment by 1
+                    int score = int.Parse(label.Text) + 1;
+                    // Update the text of the label with the incremented value
+                    label.Text = score.ToString();
+                }
+                else if (collidedGroups.Contains("Goal2"))
+                {
+                    GD.Print("Goal2");
+                    Label label = GetNode<Label>("../ScorePlayer2");
+                    // Parse the current text and increment by 1
+                    int score = int.Parse(label.Text) + 1;
+                    // Update the text of the label with the incremented value
+                    label.Text = score.ToString();
+                }
+            }
+
+            if (collidedNode is CharacterBody2D characterBody)//Bounce of player logic
             {
                 // Get the position of the ball
                 Vector2 ballPosition = Position;
 
                 // Subtract the y positions of the ball and the StaticBody2D
-                float yDifference = staticBody.Position.Y - ballPosition.Y;
-				GD.Print(yDifference);
+                float yDifference = characterBody.Position.Y - ballPosition.Y;
+                //GD.Print(yDifference);
                 // Calculate the angle based on the yDifference
                 float angle;
                 if (yDifference > 0)
